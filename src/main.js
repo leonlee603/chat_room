75e78d72cf2a43da72d1ec0178bacea6bd355d68
Leon Lee <leepingho603@gmail.com>
2025-06-08 16:41:37 +0800

    Update - Setup route guard

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { firebaseApp } from "./firebase/config";
 
-const app = createApp(App)
+let app;
 
-app.use(router)
-
-app.mount('#app')
+const auth = getAuth(firebaseApp);
+onAuthStateChanged(auth, (_user) => {
+  if (!app) {
+    app = createApp(App);
+    app.use(router);
+    app.mount("#app");
+  }
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ChatRoomView from "@/views/ChatRoomView.vue";
+import { getAuth } from "firebase/auth";
+import { firebaseApp } from "@/firebase/config";
+
+const requireAuth = (to, from, next) => {
+  let user = getAuth(firebaseApp).currentUser;
+  console.log("Current user in auth guard: ", user);
+  if (!user) {
+    next({ name: "home" });
+  } else {
+    next();
+  }
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +26,7 @@ const router = createRouter({
       path: "/chatroom",
       name: "chatroom",
       component: ChatRoomView,
+      beforeEnter: requireAuth,
     },
   ],
 });
