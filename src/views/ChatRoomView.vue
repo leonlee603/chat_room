9f931f0ddde21a36317a59f19f21b6e3d91d3080
Leon Lee <leepingho603@gmail.com>
2025-06-08 22:41:39 +0800

    Update - Handle redirect for unauthenticated user on chat room page

--- src/views/ChatRoomView.vue
+++ src/views/ChatRoomView.vue
@@ -6,9 +6,22 @@
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import getUser from "@/composables/getUser";
+import { watch } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
   components: { Navbar },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, (newUser, oldUser) => {
+      if (!user.value) {
+        router.push({ name: "home" });
+      }
+    });
+  },
 };
 </script>
 
